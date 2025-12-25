4935cc4f33dbf854a04cbcd5a5d3378193735e14
Baozxje <lamgiabao73@gmail.com>
2025-12-25 13:29:23 +0700

    them routes api cho admin

--- routes/auth.js
+++ routes/auth.js
@@ -276,4 +276,35 @@ router.post("/save-device-token", async (req, res) => {
   }
 });
 
+// lay danh sach tat ca user chi admiin 
+router.get("/users", jwtAuth, async (req, res) => {
+  try {
+    if (!["admin"].includes(req.user.role)) {
+      return res.status(403).json({ msg: "Không có quyền truy cập" });
+    }
+
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+// Lấy tất cả transaction (chỉ admin xem)
+router.get('/transactions', auth, async (req, res) => {
+  try {
+    if (req.user.role !== 'admin') {
+      return res.status(403).json({ msg: 'Access denied' });
+    }
+
+    const transactions = await Transaction.find();
+    res.json(transactions);
+  } catch (error) {
+    console.error('Error fetching transactions:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
+
 module.exports = router;
